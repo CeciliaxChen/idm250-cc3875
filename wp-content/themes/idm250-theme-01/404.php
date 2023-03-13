adc59b8c8c9d50128cfc81a721f50a1ce4fa8217
CeciliaxChen <cc3875@drexel.edu>
2023-03-13 12:50:46 -0400

    error message on the 404 page

--- wp-content/themes/idm250-theme-01/404.php
+++ wp-content/themes/idm250-theme-01/404.php
@@ -1,7 +1,7 @@
 <?php get_header('404'); ?>
 
 <div>
-    <!-- any content to denote it's a 404 error page -->
+    <h2> Oops! You may have gone to the wrong page! 
 </div>
 
 <?php get_footer('simple'); ?>
